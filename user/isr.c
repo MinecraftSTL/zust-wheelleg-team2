002667162e4d57ffedb060738a6f8a1ab73358af
MinecraftSTL <minecraftstl@163.com>
2025-10-27 16:51:57 +0800

    delete clear

--- user/isr.c
+++ user/isr.c
@@ -80,7 +80,6 @@ IFX_INTERRUPT(cc60_pit_ch0_isr, 0, CCU6_0_CH0_ISR_PRIORITY)
     }else if(switch_encoder_change_num > 0){
         pressed[NEXT_KEY] += switch_encoder_change_num;
     }
-    switch_encoder_change_num = 0;
     Fps_scan(&fps);
 }
 
